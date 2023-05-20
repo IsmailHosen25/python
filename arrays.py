875b80e32ba3addd8e87b5b1e80a9d4f8b84b5bd
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-05-20 15:57:12 +0600

    arrays

---
+++ arrays.py
@@ -0,0 +1,48 @@
+##Arry is an object that provide a mechanism for storing several data itemss with only one identifier.
+
+# a=[1,2,3,4,5,6,7,8,9,10]
+# print(a)
+
+# a=[1,2,3,4,5,6,7,8,9,10]
+# print(*a,sep=",")                ###Down 
+
+
+a=[1,2,3,4,5,6,7,8,9,10]
+for i in range(len(a)):
+     print(a[i], end=" ")       ##The end parameter is used to append any string at the end of the output of the print statement in python
+
+
+####"''''''''''''end='''''''''''''''"
+# print("Studytonight",)
+# print("is awesome")
+
+# print("Studytonight", end= "\n")
+# print("is awesome")
+##On the other hand, passing the whitespace to the end parameter indicates that the end character has to be identified by whitespace and not a new line (which is the default).
+# print("Studytonight", end=' ')
+# print("is awesome")
+
+
+##'''''''''''''''''''''''sep=''''''''''''''''''''''''
+#code for disabling the softspace feature
+# print('G','F','G', sep='')
+ 
+# #for formatting a date
+# print('09','12','2016', sep='-')
+ 
+# #another example
+# print('pratik','geeksforgeeks', sep='@')
+
+
+
+# print("Study", "tonight")
+# print("Study", "tonight", sep = ' ')
+
+# print("Study", "tonight", sep = '')
+# print("Study", "tonight", sep = ' & ')
+
+
+# print("Studytonight","has","been","created","for", sep = " _ ", end=" _STUDENTS")
+
+
+
