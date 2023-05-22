fcf4da9d9e63fe86cc67f7563a4066263e21f360
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-05-22 14:43:56 +0600

    queue

---
+++ q.py
@@ -0,0 +1,40 @@
+# queue=[]
+# queue.append("a")
+# queue.append("b")
+# queue.append("c")
+# print(queue)
+# queue.pop(0)
+# queue.pop(0)
+# print(queue)
+
+
+
+# from queue import Queue
+# q=Queue(maxsize=4)
+# print("befor inserting value ",q.qsize())
+# q.put("a")
+# q.put("b")
+# q.put("c")
+# print("q size:",q.qsize())
+# print("is q full? ",q.full())
+# print("q is empty? ", q.empty())
+# q.put("d")
+# print("Now q is full? ", q.full())
+# print(q.get())
+# print(q.get())
+# print("q is empty? ", q.empty())
+# print(q.get())
+# print(q.get())
+# print("now q should empty", q.empty())
+
+
+from collections import deque
+q=deque()
+q.append(10)
+q.append(100)
+q.append(1000)
+q.append(10000)
+print("Initial Queue is:",q)
+print(q.popleft())
+print(q.popleft())
+print("After Removing elements:",q)
