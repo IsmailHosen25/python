a0f63326cdf2a990734cc725229f1b3c13820258
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-05-24 09:07:10 +0600

    queue modified

--- q.py
+++ q.py
@@ -28,13 +28,14 @@
 # print("now q should empty", q.empty())
 
 
-from collections import deque
-q=deque()
-q.append(10)
-q.append(100)
-q.append(1000)
-q.append(10000)
-print("Initial Queue is:",q)
-print(q.popleft())
-print(q.popleft())
-print("After Removing elements:",q)
+# from collections import deque
+# q=deque()
+# q.append(10)
+# q.append(100)
+# q.append(1000)
+# q.append(10000)
+# print("Initial Queue is:",q)
+# print(q.popleft())
+# print(q.popleft())
+# print("After Removing elements:",q)
+
