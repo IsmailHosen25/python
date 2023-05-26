72cfa365ff67966fb866df97680e2f645a9cd81f
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-05-26 12:09:03 +0600

    stringfunction

---
+++ stringfunction.py
@@ -0,0 +1,28 @@
+# a="hello hasan, how are you?"
+# print(a.capitalize())
+
+
+# a="HELLO Hasan, How are you?"
+# # print(a.casefold())
+# print(a.lower())
+
+
+# a="Hasan"
+# print(a.center(11,'*'))
+# print(a.center(9,'$'))
+# print(a.center(13,'#'))
+
+
+# a="hello hasan, how are you?"
+# print(a.count("y"))
+# print(a.count("h"))
+# print(a.count("how"))
+# print(a.count("o"))
+# print(a.count("o", 1,5))             ###those number are index of index number
+# print(a.count("o", 6,-1))
+
+
+# a="hello hasan, how are you?"
+# print(a.find("hasan"))               ###those number are index of index number
+# print(a.find("you"))
+# print(a.find("hasan", 15,40))
