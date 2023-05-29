83730194b7fd98758afa9c19343dd149066bb3ae
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-05-29 22:38:32 +0600

    arratyfunction added

---
+++ arrayfunction.py
@@ -0,0 +1,124 @@
+####This is append function, this function are used for adding new value for array
+
+# h=["h",'c','d']
+# print(h)
+# h.append("f")
+# print(h)
+
+# li=[]
+#print(li)
+# li.append("yes")
+# li.append("no")
+# li.append("yeah")
+# li.append("nope")
+# print(li)
+
+
+
+##pop function remove value of list from right
+
+# h=["h",'c','d',"n",'d']
+# print(h)
+# h.pop()
+# print(h)
+# h.pop()
+# print(h)
+
+# h=["h",'c','d',"n",'d']
+# print(h)
+# h.pop(3)
+# print(h)
+# h.pop(1)
+# print(h)
+
+
+### copy function is used for coping a list
+
+# h=["h",'c','d',"n",'d']
+# print(h)
+# alpa=h.copy()
+# print(alpa)
+
+
+
+###### clear function is used for clearing whole list value
+
+# h=["h",'c','d',"n",'d']
+# print(h)
+# h.clear()
+# print(h)
+
+
+
+
+###### index function is used for knoing a index of a value in list
+
+# h=["h",'c','d','a','n']
+# print(h.index("d"))
+# h.append("k")
+# print(h.index("k"))
+
+
+### extend function is used for add a list to another list or tuple
+
+# h=["h",'c','d','a','n']
+# l=["m",'z']
+# print(h)
+# print(l)
+# h.extend(l)
+# print(h)
+
+#### insert function is used for insert a value in a spacific index in list
+
+# h=["h",'c','d','a','n']
+# print(h)
+# h.insert(2,"p")
+# print(h)
+
+
+#### Remove function remove list value 
+
+# h=["h",'c','d','a','n']
+# print(h)
+# h.remove("c")
+# print(h)
+
+
+# h=["h",'c','d','a',"c",'n',"c"]
+# print(h)
+# h.remove("c")
+# print(h)
+
+##### count function count the value exiting in list and tuple
+
+# h=["h",'c','d','a','n']
+# print(h)
+# c=h.count("c")
+# print(c)
+# print()
+# h=["h",'c','d','a',"c",'n',"c"]
+# print(h)
+# c=h.count("c")
+# print(c)
+
+
+######Reverse function revearse the whole list
+
+# h=["h",'c','d','a',"c",'n',"c"]
+# print(h)
+# h.reverse()
+# print(h)
+
+#####sort function is used for sorting the list in ascending order
+
+# h=["h",'c','d','a',"c",'n',"c"]
+# print(h)
+# h.sort()
+# print(h)
+# num=['1','6','3','9','7']
+# print(num)
+# num.sort()
+# print(num)
+
+
+
